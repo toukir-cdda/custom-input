aa18be8bf6a64fd062cb7224e46e63261b469f35
Toukir Ahmed Raju <toukir@cdda.io>
2023-09-11 18:15:04 +0600

    create new table

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,15 +1,225 @@
-import { cards } from "./data";
-import Card from "../components/Card";
+import {
+  AiOutlineArrowDown,
+  AiOutlineEye,
+  AiOutlineArrowLeft,
+  AiOutlineArrowRight,
+} from "react-icons/ai";
 
 export default function Dashboard() {
   return (
     <div className="bg-gray-400 h-screen pl-[250px] pt-[84px] ">
       {/* container */}
-      <div className="bg-slate-50 h-full p-4 flex gap-4">
+      <div className="bg-slate-50 h-full p-4 ">
         {/* card */}
-        {cards.map((card, index) => (
+        {/* {cards.map((card, index) => (
           <Card key={index} data={card} />
-        ))}
+        ))} */}
+        <div className="bg-white rounded-md h-full shadow-md p-6 flex flex-col gap-6">
+          {/* header  */}
+          <div className="flex flex-col gap-2">
+            <h3 className="text-2xl font-medium">Transaction Activity</h3>
+            <span className="text-sm font-normal text-gray-600">
+              This page contains all of Transaction Activity
+            </span>
+          </div>
+          <hr />
+          {/* transaction table  */}
+
+          <div className="relative overflow-hidden rounded-md border-gray-100 border-[1px] ">
+            <table className="w-full text-left text-gray-500 ">
+              {/* table header  */}
+              <thead className=" text-[12px] text-gray-700 capitalize bg-gray-100  ">
+                <tr>
+                  <th scope="col" className="px-4 py-3">
+                    <div className="flex gap-1 items-center text-xs font-normal text-gray-500">
+                      <span>Offer Date & ID</span>
+                      <AiOutlineArrowDown />
+                    </div>
+                  </th>
+                  <th scope="col" className="px-4 py-3">
+                    <span className="text-xs font-normal text-gray-500">
+                      Status
+                    </span>
+                  </th>
+                  <th scope="col" className="px-4 py-3">
+                    <span className="text-xs font-normal text-gray-500">
+                      Property Address
+                    </span>
+                  </th>
+                  <th scope="col" className="px-4 py-3">
+                    <span className="text-xs font-normal text-gray-500">
+                      Offer Amount
+                    </span>
+                  </th>
+                  <th scope="col" className="px-4 py-3">
+                    <span className="text-xs font-normal text-gray-500">
+                      Seller
+                    </span>
+                  </th>
+                  <th scope="col" className="px-4 py-3">
+                    <span className="text-xs font-normal text-gray-500">
+                      Buyer
+                    </span>
+                  </th>
+                  <th scope="col" className="px-4 py-3">
+                    <span className="text-xs font-normal text-gray-500">
+                      Action
+                    </span>
+                  </th>
+                </tr>
+              </thead>
+              {/* table body  */}
+              <tbody className="divide-y divide-gray-200">
+                {/* table row start */}
+                {Array(7)
+                  .fill(0)
+                  .map((_, index) => (
+                    <tr className="bg-white  font-medium text-gray-700 text-[12px] ">
+                      <td
+                        key={index}
+                        scope="row"
+                        className="px-4 py-3 font-medium wrap "
+                      >
+                        <div className="flex flex-col">
+                          <span className="text-sm font-semibold">
+                            PS-2023-05-269877
+                          </span>
+                          <span className="text-sm font-medium text-gray-500">
+                            Jan 6, 2023
+                          </span>
+                        </div>
+                      </td>
+                      <td className="px-4 py-3">
+                        <span className="rounded-xl text-sm font-normal bg-orange-200 px-2 py-0.5 text-orange-500">
+                          Pending
+                        </span>
+                      </td>
+                      <td className="px-4 py-3">
+                        <span className="text-sm font-medium">
+                          1901 Thornridge Cir. Shiloh, Hawaii 81063
+                        </span>
+                      </td>
+                      <td className="px-4 py-3">
+                        <span className="text-sm font-medium">$900.00</span>
+                      </td>
+                      <td className="px-4 py-3">
+                        <div className="flex flex-col">
+                          <span className="text-sm font-medium">
+                            Wade Warren
+                          </span>
+                          <span className="text-xs font-normal text-gray-500">
+                            wadewarren@property.app
+                          </span>
+                          <span className="text-xs font-normal text-gray-500">
+                            (205) 555-0100
+                          </span>
+                        </div>
+                      </td>
+                      <td className="px-4 py-3">
+                        <div className="flex flex-col">
+                          <span className="text-sm font-medium">
+                            Wade Warren
+                          </span>
+                          <span className="text-xs font-normal text-gray-500">
+                            wadewarren@property.app
+                          </span>
+                          <span className="text-xs font-normal text-gray-500">
+                            (205) 555-0100
+                          </span>
+                        </div>
+                      </td>
+                      <td className="px-4 py-3">
+                        <div className="flex flex-col">
+                          <span className="text-[10px] font-normal text-gray-500">
+                            Lest View Date 09-11-23
+                          </span>
+                          <button className="flex items-center justify-center py-2 gap-1 rounded-md bg-teal-700 text-white">
+                            <AiOutlineEye />
+                            <span className="text-xs font-normal">
+                              View Details
+                            </span>
+                          </button>
+                        </div>
+                      </td>
+                    </tr>
+                  ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+
+        {/* new table  */}
+
+        <table className="w-full flex flex-row flex-no-wrap sm:bg-white rounded-lg overflow-hidden sm:shadow-lg my-5">
+          <thead className="text-white">
+            <tr className="bg-teal-400 flex flex-col flex-no wrap sm:table-row rounded-l-lg sm:rounded-none mb-2 sm:mb-0">
+              <th className="p-3 text-left">Name</th>
+              <th className="p-3 text-left">Email</th>
+              <th className="p-3 text-left">Actions</th>
+            </tr>
+            <tr className="bg-teal-400 flex flex-col flex-no wrap sm:table-row rounded-l-lg sm:rounded-none mb-2 sm:mb-0">
+              <th className="p-3 text-left">Name</th>
+              <th className="p-3 text-left">Email</th>
+              <th className="p-3 text-left">Actions</th>
+            </tr>
+            <tr className="bg-teal-400 flex flex-col flex-no wrap sm:table-row rounded-l-lg sm:rounded-none mb-2 sm:mb-0">
+              <th className="p-3 text-left">Name</th>
+              <th className="p-3 text-left">Email</th>
+              <th className="p-3 text-left">Actions</th>
+            </tr>
+            <tr className="bg-teal-400 flex flex-col flex-no wrap sm:table-row rounded-l-lg sm:rounded-none mb-2 sm:mb-0">
+              <th className="p-3 text-left">Name</th>
+              <th className="p-3 text-left">Email</th>
+              <th className="p-3 text-left">Actions</th>
+            </tr>
+          </thead>
+          <tbody className="flex-1 sm:flex-none">
+            <tr className="flex flex-col flex-no wrap sm:table-row mb-2 sm:mb-0">
+              <td className="border-grey-light border hover:bg-gray-100 p-3">
+                John Covv
+              </td>
+              <td className="border-grey-light border hover:bg-gray-100 p-3 truncate">
+                contato@johncovv.com
+              </td>
+              <td className="border-grey-light border hover:bg-gray-100 p-3 text-red-400 hover:text-red-600 hover:font-medium cursor-pointer">
+                Delete
+              </td>
+            </tr>
+            <tr className="flex flex-col flex-no wrap sm:table-row mb-2 sm:mb-0">
+              <td className="border-grey-light border hover:bg-gray-100 p-3">
+                Michael Jackson
+              </td>
+              <td className="border-grey-light border hover:bg-gray-100 p-3 truncate">
+                m_jackson@mail.com
+              </td>
+              <td className="border-grey-light border hover:bg-gray-100 p-3 text-red-400 hover:text-red-600 hover:font-medium cursor-pointer">
+                Delete
+              </td>
+            </tr>
+            <tr className="flex flex-col flex-no wrap sm:table-row mb-2 sm:mb-0">
+              <td className="border-grey-light border hover:bg-gray-100 p-3">
+                Julia
+              </td>
+              <td className="border-grey-light border hover:bg-gray-100 p-3 truncate">
+                julia@mail.com
+              </td>
+              <td className="border-grey-light border hover:bg-gray-100 p-3 text-red-400 hover:text-red-600 hover:font-medium cursor-pointer">
+                Delete
+              </td>
+            </tr>
+            <tr className="flex flex-col flex-no wrap sm:table-row mb-2 sm:mb-0">
+              <td className="border-grey-light border hover:bg-gray-100 p-3">
+                Martin Madrazo
+              </td>
+              <td className="border-grey-light border hover:bg-gray-100 p-3 truncate">
+                martin.madrazo@mail.com
+              </td>
+              <td className="border-grey-light border hover:bg-gray-100 p-3 text-red-400 hover:text-red-600 hover:font-medium cursor-pointer">
+                Delete
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   );
--- hooks/useCustomForm.ts
+++ hooks/useCustomForm.ts
@@ -7,18 +7,20 @@ interface ValidationRules {
   maxLength?: number;
   email?: boolean;
   phone?: boolean;
-  // Add more custom validation rules as needed
   customMessage?: string;
 }
 
 interface FieldRegistration {
   name: string;
   onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  onBlur: () => void;
+  touched: boolean;
 }
 
 interface CustomForm {
   formData: Record<string, string>;
   formErrors: Record<string, string>;
+  touchedFields: Record<string, boolean>;
   register: (
     fieldName: string,
     validationRules?: ValidationRules
@@ -27,13 +29,12 @@ interface CustomForm {
 }
 
 export function useCustomForm(): CustomForm {
-  // State to hold form data
   const [formData, setFormData] = useState<Record<string, string>>({});
-
-  // State to hold form errors
   const [formErrors, setFormErrors] = useState<Record<string, string>>({});
+  const [touchedFields, setTouchedFields] = useState<Record<string, boolean>>(
+    {}
+  );
 
-  // Function to register a form field and set up validation
   const register = (
     fieldName: string,
     validationRules: ValidationRules = {}
@@ -45,10 +46,14 @@ export function useCustomForm(): CustomForm {
         setFormData({ ...formData, [name]: value });
         validateField(name, value, validationRules);
       },
+      onBlur: () => {
+        setTouchedFields({ ...touchedFields, [fieldName]: true });
+        validateAllFields();
+      },
+      touched: !!touchedFields[fieldName],
     };
   };
 
-  // Function to validate a specific form field
   const validateField = (
     fieldName: string,
     value: string,
@@ -86,39 +91,43 @@ export function useCustomForm(): CustomForm {
     setFormErrors({ ...formErrors, ...errors });
   };
 
-  // Function to validate an email address
+  const validateAllFields = () => {
+    // Validate all fields when onBlur is triggered
+    Object.keys(formData).forEach((fieldName) => {
+      const value = formData[fieldName];
+      const validationRules = {}; // Define your validation rules here for each field
+      validateField(fieldName, value, validationRules);
+    });
+  };
+
   const isValidEmail = (email: string): boolean => {
     // Implement your email validation logic here
-    // For a basic check, you can use a regex pattern
     const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
     return emailRegex.test(email);
   };
 
-  // Function to validate a phone number
   const isValidPhoneNumber = (phone: string): boolean => {
     // Implement your phone number validation logic here
-    // For a basic check, you can use a regex pattern
     const phoneRegex = /^\d{10}$/; // Change this pattern according to your validation rules
     return phoneRegex.test(phone);
   };
 
-  // Function to handle form submission
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
-    // Add any final validation logic here if needed
+    validateAllFields();
 
     // Check for form-wide validation errors before submitting
     const hasErrors = Object.keys(formErrors).length > 0;
     if (!hasErrors) {
       // Submit the form
       // You can add your submission logic here
-      console.log("Form submitted with data:", formData);
     }
   };
 
   return {
     formData,
     formErrors,
+    touchedFields,
     register,
     handleSubmit,
   };
